20a051441bd16da0bca1fb04c02df135bd6bcf45
mkalog22 <149401798+mkalog22@users.noreply.github.com>
2024-07-31 17:27:05 +0300

    Update server.js

--- node-app/server.js
+++ node-app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the other side! Node.js');
+  res.send('Hello from the other side!');
 });
 
 app.listen(port, () => {
